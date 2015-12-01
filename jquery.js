c33bdf87bc0383efb63f7736824ec58574d3bb88
vakulchyk <e.vakulchyk@gmail.com>
2015-12-01 14:48:01 +0700

    draft properly working slider, no close onclick no pointer ets

---
+++ jquery.js
@@ -0,0 +1,126 @@
+$(document).ready(function(){
+
+	var iconHeight = 98;
+	
+	
+	 
+	// hideShow (thisSlider);
+	// hideShow (".sliderPod");
+	// hideShow (".sliderMood");
+	// hideShow ("sliderWish");
+	
+	$(".slider").mouseleave(closeSlider);
+	
+	$(".btnUp").click(function() {
+		rotateUp( $(this) );
+		var thisSlider = $(this).parent();
+		hideShow(thisSlider);	
+	})
+	
+	$(".btnDown").click(function(){
+		rotateDown( $(this) );
+		var thisSlider = $(this).parent();
+		hideShow(thisSlider);
+	})
+	
+	//chose of icon by click on it
+	$(".icon").click(function(){
+		var nbrOfIconClicked = parseInt($(this).css('top')) / iconHeight;
+		if (nbrOfIconClicked == 0) {
+			closeSlider; rotateDown( $(this) );
+		} else if (nbrOfIconClicked == 1){
+			closeSlider;
+		} else {
+			closeSlider; rotateUp( $(this) ); 
+		}
+		var thisSlider = $(this).parent();
+		hideShow(thisSlider);
+	})
+	
+	//ordering slider
+	// var iconHeight = parseInt($('.icon').height());
+	$(".icon").each(function(){
+		$(this).css("top", iconHeight*($(this).index()))
+		// console.log(iconHeight*($(this).index()));
+	});
+	
+	//opening slider
+	$(".slider").mouseenter(function(){
+		$(this).parent().removeClass("inactive").addClass("active");
+		$(this).css("top", "0px");
+		hideShow($(this));
+	});
+	
+	//hiding icons
+	var sliders = $(this).find(".slider");
+	sliders.each(function(){
+		hideShow($(this));
+	})
+	
+	//closing slider
+	function closeSlider(thisObj){
+		$(this).parent().removeClass("active").addClass("inactive");
+		$(this).css("top", "-101px")
+		hideShow($(this));
+	}
+	
+	//control with arrows
+	function rotateUp(thisObj){
+		var parnt = thisObj.parent();
+		// var iconHeight = parseInt($('.icon').height()); //not working properly
+		var iconHeight = 98;
+		var arrHeight = ($(parnt).find('.icon').length - 1) * iconHeight;
+		$(parnt).find('.icon').each(function(){
+			var top = parseInt($(this).css('top'));
+			if (top < iconHeight) {
+					$(this).css('top', arrHeight);
+				} 
+				else {
+					$(this).css('top', top - iconHeight);
+			}
+		});
+	}
+	
+	function rotateDown(thisObj){
+		var parnt = $(thisObj).parent();
+		var iconHeight = 98;
+		var arrHeight = ($(parnt).find('.icon').length - 1) * iconHeight;
+		$(parnt).find('.icon').each(function(){
+			var top = parseInt($(this).css('top'));
+			if (top >= arrHeight) {
+				$(this).css('top', '0');
+			} 
+			else {
+				$(this).css('top', top + iconHeight);
+			}
+		});
+	}
+	
+	function hideShow(thisObj){
+		var icons = thisObj.children('.icon');
+		console.log(icons);
+		if (thisObj.parent().hasClass("inactive") == false){
+			icons.each(function(){
+				$(this).css("visibility", 'hidden');
+				if (parseInt($(this).css('top')) == 0){
+					$(this).css("visibility", 'visible');
+				}
+				else if (parseInt($(this).css('top')) == iconHeight){
+					$(this).css("visibility", 'visible');
+				}
+				else if (parseInt($(this).css('top')) == (iconHeight * 2)){
+					$(this).css("visibility", 'visible');
+				} 
+			});	
+		}
+		else {
+			icons.each(function(){
+				$(this).css("visibility", 'hidden');
+				console.log($(this).css('top'));
+				if (parseInt($(this).css('top')) == iconHeight){
+					$(this).css("visibility", 'visible');
+				}
+			});	
+		}
+	}
+})
